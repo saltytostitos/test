b011f94e675db9938dad2233228712b94db54f6e
saltytostitos <taxable.honeyed_06@icloud.com>
2024-08-24 22:42:44 -0500

    INIT

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+  <UPageBody>
+    <UContainer class="space-y-8">
+      <UAuthForm icon="i-heroicons-user-circle" title="Login" :fields>
+        <template #password-hint>
+          <UButton disabled variant="link" color="gray"
+            >Forgot password?</UButton
+          >
+        </template>
+        <template #footer>
+          <div>
+            By signing in, you agree to our
+            <NuxtLink disabled class="text-primary font-medium"
+              >Terms of Service</NuxtLink
+            >.
+          </div>
+        </template>
+      </UAuthForm>
+    </UContainer>
+  </UPageBody>
+</template>
+
+<script setup lang="ts">
+const fields = [
+  {
+    name: "email",
+    type: "email",
+    label: "Email",
+  },
+  {
+    name: "password",
+    type: "password",
+    label: "Password",
+  },
+  {
+    name: "remember",
+    label: "Remember me",
+    type: "checkbox",
+  },
+];
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  modules: ["@nuxt/ui"],
+  extends: ["@nuxt/ui-pro"],
+  devtools: { enabled: true },
+});
